2c299396e4145cbf7a6de166c2d91bfcaba7fd66
AtaraxiaCodes <rayhnouf@gmail.com>
2020-09-16 15:03:53 +0000

    Customized for Repl.it and Chill Cafe

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,16 @@
 const Eris = require('eris');
 const config = require('./config');
 
+const http = require('http');
+const server = http.createServer((req, res) => {
+	res.writeHead(200);
+	res.end('Chill Cafe Mod Mail Running');
+});
+server.listen(3000);
+
 const bot = new Eris.Client(config.token, {
-  getAllUsers: true,
-  restMode: true,
+	getAllUsers: true,
+	restMode: true
 });
 
 module.exports = bot;
